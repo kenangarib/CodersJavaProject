ab9e0d3e223c1d40010b3893a465fbd4dc815e37
Predator2000 <kenan.garibzadeh@gmail.com>
2022-09-23 19:48:39 +0400

    bootcamp

---
+++ src/BootCamp.java
@@ -0,0 +1,12 @@
+import com.intellij.psi.PsiYieldStatement;
+
+public class BootCamp {
+    public static void main(String[] args) {
+
+        String[] mobiles =  { "Samsung","Iphone","Huawei","Oppo","Oneplus"};
+
+        for (int i = 0; i < mobiles.length; i++) {
+            System.out.println(mobiles[i]);
+        }
+    }
+}
--- src/cwcwcw.java
+++
@@ -1,5 +0,0 @@
-public class cwcwcw {
-    public static void main(String[] args) {
-        System.out.println("ekdekdne");
-    }
-}
